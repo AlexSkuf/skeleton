099ce2d066c47fc0ba15101e37761fb6c6483418
Alex Skuf <91368390+AlexSkuf@users.noreply.github.com>
2022-01-22 23:52:33 +0200

    Two-sections

--- assets/theme.js
+++ assets/theme.js
@@ -1,7 +1,12 @@
 let myImageSlider = new Swiper('.slider-image');
 
 
-let myTextSlider = new Swiper('.slider-text');
+let myTextSlider = new Swiper('.slider-text', {
+  navigation: {
+    nextel: '.swiper-button-next',
+    prevEl: '.swiper button-prev'
+  },
+});
 
 myImageSlider.controller.control = myTextSlider;
 myTextSlider.controller.control = myImageSlider;
